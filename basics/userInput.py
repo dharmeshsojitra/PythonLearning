b0432b4049c94e5adc2235cd0d15f0edbca9d853
Dharmesh Sojitra <dharmeshsojitra@Ds-World-2.local>
2025-05-26 17:49:26 +0530

    added user input and varibales code

---
+++ basics/userInput.py
@@ -0,0 +1,6 @@
+print("enter your name: ")
+name = input()
+print("enter your age: ")
+age = input()
+print("your name is:", name)
+print("your age is:", age)
---
+++ basics/variables.py
@@ -0,0 +1,2 @@
+print("price of 1 apple is 40")
+qty = input("how many apple you want? ")
