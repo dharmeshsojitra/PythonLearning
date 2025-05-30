3cd2f2919a67ae673f99f893201ae6158971dd8f
Dharmesh Sojitra <dharmeshsojitra@Ds-World-2.local>
2025-05-30 18:28:47 +0530

    loop implementaion and sum of price

---
+++ basics/add_amount_loops.py
@@ -0,0 +1,5 @@
+price = [10,20,30,50]
+total =0
+for item in price:
+    total = total + item
+print(total)
--- basics/loops.py
+++ basics/loops.py
@@ -1,2 +1,8 @@
 for items in [1,2,3,4,5]:
     print(items)
+for items in 'for loop':
+    print(items)
+for item in  range(10):
+    print(item)
+for item in range(0,50,5):
+    print(item)
