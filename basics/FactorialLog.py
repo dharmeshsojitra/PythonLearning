78b3d05914afa760dc523a4e9ba7293bf11487e2
Dharmesh Sojitra <dharmeshsojitra@Ds-World-2.local>
2025-06-11 17:10:31 +0530

    fixed the logic to calculate fatorial

--- basics/FactorialLog.py
+++ basics/FactorialLog.py
@@ -4,12 +4,15 @@ logging.debug('start of the program')
 
 def factorial(n):
     logging.debug('start of the factorial('+str(n)+')')
-    total = 1;
+    total = 1
     for i in range(n+1):
+        if i ==0:
+            continue
         total *=i
         logging.debug('i is'+str(i)+ ', total is'+str(total))
     logging.debug('end of the factorial('+str(n)+')')
     return total
+
 
 print(factorial(5))
 
