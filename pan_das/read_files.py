4eb9a201e7f7820ac42124f6fe1b8b245697b7a0
Dharmesh Sojitra <dharmeshsojitra@Ds-World-2.local>
2025-06-24 11:24:15 +0530

    refactored sample data to dataset directory

--- pan_das/read_files.py
+++ pan_das/read_files.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import xlrd
 
-json_content = pd.read_json("../sample_Data.json")
+json_content = pd.read_json("../datasets/sample_Data.json")
 print(json_content)
 
 
-excel_content = pd.read_excel("../SampleSuperstore.xlsx")
+excel_content = pd.read_excel("../datasets/SampleSuperstore.xlsx")
 print(excel_content)
 
 
-csv_content = pd.read_csv("../sales_data_sample.csv", encoding="latin1")
+csv_content = pd.read_csv("../datasets/sales_data_sample.csv", encoding="latin1")
 print(csv_content)
