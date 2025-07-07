cf0cba104d432d1afe7a16f031b942499ae3bfb8
Dharmesh Sojitra <dharmeshsojitra@Ds-World-2.local>
2025-07-07 11:43:24 +0530

    created numpy array with existing python list

---
+++ num_py/1d_arrays.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+one_d = np.array([1,2,3])
+print(one_d)
+
+
+print('type of the array is using type(): ', type(one_d))
+print('type of the one_d array is: ',one_d.dtype)
+
+print('is one_d array type of str? : ', isinstance(one_d, str))
