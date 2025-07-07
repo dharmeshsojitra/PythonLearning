364a6ca784e6b312c165ba9dfdb2634db768e67f
Dharmesh Sojitra <dharmeshsojitra@Ds-World-2.local>
2025-07-07 11:47:54 +0530

    created np arrays with arange function

--- num_py/1d_arrays.py
+++ num_py/1d_arrays.py
@@ -8,3 +8,16 @@ print('type of the array is using type(): ', type(one_d))
 print('type of the one_d array is: ',one_d.dtype)
 
 print('is one_d array type of str? : ', isinstance(one_d, str))
+
+
+print('*' * 15 +' using arange() to create a np array' + '*' * 15)
+
+range_array = np.arange(10)
+print(range_array)
+
+
+range_array = np.arange(2, 10)
+print(range_array)
+
+range_array = np.arange(2, 10, 2)
+print(range_array)
